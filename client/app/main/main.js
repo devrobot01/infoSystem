d4713a424aaf6f6270e7fc27a3b03bf5d9eee226
devrobot01 <alex@devrobot.de>
2015-11-05 17:44:37 +0100

    Added angular-fullstack

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('infoSystemApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
