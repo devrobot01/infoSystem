f0566e8c8ec45e3d7d525642cdfa93540ca3127f
devrobot01 <alex@devrobot.de>
2015-11-06 14:34:21 +0100

    Added simple weather forecast and dateTime implementation with hardcoded data

--- client/app/dateTime/dateTime.controller.js
+++ client/app/dateTime/dateTime.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('infoSystemApp')
-  .controller('DateTimeCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    .controller('DateTimeCtrl', function ($scope) {
+        $scope.dateTime = {
+            date: '21.10.2015',
+            time: '13:37',
+            calendar: 'calendarView'
+        };
+    });
--- client/app/weather/weather.controller.js
+++ client/app/weather/weather.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('infoSystemApp')
-  .controller('WeatherCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    .controller('WeatherCtrl', function ($scope) {
+        $scope.weather = {
+            windsun: 'sunny',
+            temp: '30c',
+            forecast:'15c'
+        };
+    });
