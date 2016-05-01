8e9c3e5afe76a12812808aacb157379326b4ec87
Alexander Giesa <info@1-buy.de>
2016-05-01 21:36:31 +0200

    gets data from port but with errors

--- client/app/terminal/terminal.controller.js
+++ client/app/terminal/terminal.controller.js
@@ -3,81 +3,144 @@
 angular.module('infoSystemApp')
     .controller('TerminalCtrl', function ($scope, Modal, Auth, socket, $http) {
 
-        $scope.icon = {
-            uv: {
-                true: "assets/images/pictos/ic_check_box_24px.svg",
-                false: "assets/images/pictos/ic_check_box_outline_blank_24px.svg",
-            },
-                270: "images/pictos/ic_arrow_forward_24px.svg",
-                360: "images/pictos/ic_vertical_align_bottom_24px.svg"
-        };
-
-        //var socket = io.connect('localhost:9000');
-
-       /*socket.on('count', function (data) {
-            console.log(data);
-        });*/
+        $scope.awesomeThings = {};
 
         $http.get('/api/serialports').success(function(awesomeThings) {
             $scope.awesomeThings = awesomeThings;
             socket.syncUpdates('serialport', $scope.awesomeThings);
             console.log($scope.awesomeThings);
+            $scope.updateVal();
         });
 
+        $scope.addGroup = function(){
+            var group = {
+                group: [{
+                    power: false,
+                    uv: false,
+                    powerOn_hours: '0',
+                    service_code: -1,
+                    service_date: '',
+                    error_code: -1,
+                    error_date: ''
+                }, {
+                    power: false,
+                    uv: false,
+                    powerOn_hours: '0',
+                    service_code: -1,
+                    service_date: '',
+                    error_code: -1,
+                    error_date: ''
+                }, {
+                    power: false,
+                    uv: false,
+                    powerOn_hours: '0',
+                    service_code: -1,
+                    service_date: '',
+                    error_code: -1,
+                    error_date: ''
+                }]
+            };
+            $scope.deviceList.push(group);
+        };
         $scope.deviceList = [{
             group:[
-            {
-                power: false,
-                uv: false,
-                powerOn_hours: '0',
-                service_code: -1,
-                service_date: '',
-                error_code: -1,
-                error_date: ''
-            },{
-                power: false,
-                uv: false,
-                powerOn_hours: '0',
-                service_code: -1,
-                service_date: '',
-                error_code: -1,
-                error_date: ''
-            },{
-                power: false,
-                uv: false,
-                powerOn_hours: '0',
-                service_code: -1,
-                service_date: '',
-                error_code: -1,
-                error_date: ''
-            }]},{
-            group:[
-            {
-                power: false,
-                uv: false,
-                powerOn_hours: '2',
-                service_code: 3,
-                service_date: '',
-                error_code: -1,
-                error_date: ''
-            }, {
-                power: true,
-                uv: false,
-                powerOn_hours: '3',
-                service_code: -1,
-                service_date: '',
-                error_code: -1,
-                error_date: ''
-            }, {
-                power: false,
-                uv: false,
-                powerOn_hours: '4',
-                service_code: -1,
-                service_date: '',
-                error_code: -1,
-                error_date: ''
-            }]
+                {
+                    power: false,
+                    uv: false,
+                    powerOn_hours: '0',
+                    service_code: -1,
+                    service_date: '',
+                    error_code: -1,
+                    error_date: ''
+                },{
+                    power: false,
+                    uv: false,
+                    powerOn_hours: '0',
+                    service_code: -1,
+                    service_date: '',
+                    error_code: -1,
+                    error_date: ''
+                },{
+                    power: false,
+                    uv: false,
+                    powerOn_hours: '0',
+                    service_code: -1,
+                    service_date: '',
+                    error_code: -1,
+                    error_date: ''
+                }]
         }];
+
+        $scope.updateVal = function() {
+            for (var key in $scope.awesomeThings) {
+                var _modul= parseInt($scope.awesomeThings[key].modul)-1;
+                // TODO: wenn zB 1 und 3 belegt sind gibt es fehler weil modul 2 fehlt
+                if ($scope.awesomeThings[key].value[3] == '00') {
+                    if (angular.isUndefined($scope.deviceList[_modul])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[_modul].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[_modul].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[_modul].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '01') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '02') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '03') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '04') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '05') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '06') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+                if ($scope.awesomeThings[key].value[3] == '07') {
+
+                    if (angular.isUndefined($scope.deviceList[key])) { $scope.addGroup(); }
+                    //hex stundenzahl in int wandeln
+                    $scope.deviceList[key].group[0].powerOn_hours = $scope.awesomeThings[key].value[5];
+                    $scope.deviceList[key].group[1].powerOn_hours = $scope.awesomeThings[key].value[7];
+                    $scope.deviceList[key].group[2].powerOn_hours = $scope.awesomeThings[key].value[9];
+                }
+            }
+        };
+
         $scope._counter = 0;
         $scope.go = function(targetId){
             if(targetId >= 0 && targetId < $scope.deviceList.length) {
--- server/api/serialport/serialport.controller.js
+++ server/api/serialport/serialport.controller.js
@@ -5,6 +5,7 @@ var Serialport = require('./serialport.model');
 var module = [0x01,0x02,0x03,0x04,0x05,0x06,0x07];
 var SerialPort = require('serialport').SerialPort;
 var port = new SerialPort("/dev/ttyUSB0", { baudrate: 9600 },false);
+var sleep = require('sleep');
 
 var response = {
   STX:'', // Start Zeichen 02
@@ -20,7 +21,7 @@ var response = {
 
 var serialportsList;
 
-function loadList() {
+function loadSerialportsList() {
   Serialport.find(function (err, serialports) {
     if (err) { return handleError(res, err); }
     return serialportsList = serialports.slice(0);
@@ -28,102 +29,139 @@ function loadList() {
 }
 
 (function (){
-  loadList();
+  loadSerialportsList();
+  var dataArray = [];
+  // wascheinlich besser den port per funktion zu öffen und den entsprechende buffer zu übergeben
+
+  setInterval(function(){
   port.open(function (err) {
     if (err) {
       return console.log('Error opening port: ', err.message);
     }
     /*function getZaehlerstand(module){
-      for(var modul in module){
-        console.log(module[modul]);
-        // 02 = start, 01 = modul, 01 = modul, 00 = länge der Datenbits bei anfrage immer 00, 00 = Zählerstände Senden, 05 = TYP Anfrage
-        port.write(new Buffer([0x02, module[modul], 0x01, 0x00, 0x00, 0x05]));
+     for(var modul in module){
+     console.log(module[modul]);
+     // 02 = start, 01 = modul, 01 = modul, 00 = länge der Datenbits bei anfrage immer 00, 00 = Zählerstände Senden, 05 = TYP Anfrage
+     port.write(new Buffer([0x02, module[modul], 0x01, 0x00, 0x00, 0x05]));
+     }
+     }
+
+     getZaehlerstand(module);*/
+
+
+      console.log('ask for count 0');
+      var bufferArray = [
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x00, 0x05]),
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x01, 0x05]),
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x02, 0x05]),
+        /*new Buffer([0x02, 0x01, 0x01, 0x00, 0x03, 0x05]),
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x04, 0x05]),
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x05, 0x05]),
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x06, 0x05]),
+        new Buffer([0x02, 0x01, 0x01, 0x00, 0x07, 0x05])*/
+      ];
+      for(var buffer in bufferArray){
+        console.log('ask for count: ' + buffer);
+        port.write(bufferArray[buffer]);
+        sleep.sleep(6);
       }
-    }
 
-    getZaehlerstand(module);*/
-    setInterval(function(){
-      console.log('ask for count');
-      port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x00, 0x05]));
-      var dataArray = [];
+      /*console.log('ask for count 1');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x01, 0x05])),200);
+      console.log('ask for count 2');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x02, 0x05])),300);
+      console.log('ask for count 3');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x03, 0x05])),400);
+      console.log('ask for count 4');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x04, 0x05])),500);
+      console.log('ask for count 5');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x05, 0x05])),600);
+      console.log('ask for count 6');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x06, 0x05])),700);
+      console.log('ask for count 7');
+      setTimeout(port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x07, 0x05])),800);*/
 
-      port.on('data', function(data) {
-        //console.log('data received: ' + data.toString('hex'));
-        var stringData = data.toString('hex');
-        /*
-         * var jsonData ist ein array also kann mit jsonData[0] auf die verschiedenen values zugegriffen werden.
-         */
-        var jsonData = stringData.match(/.{1,2}/g);
-        //console.log('jsonData: ' + jsonData);
-
-        var key, count = 0;
-        for(key in jsonData) {
-          if(jsonData.hasOwnProperty(key)) {
-            dataArray.push(jsonData[count]);
-            count++;
-          }
-        }
-        //console.log('count: ' + count);
-        // console.log(response);
-
-        if(dataArray[11] != null && dataArray[12] == null) {
-          //console.log('Data: ' + dataArray);
-          var newThing = {};
-          newThing.modul = dataArray[1];
-          newThing.type = dataArray[3];
-          newThing.value = dataArray;
-          var res;
-
-          loadList();
-          var _modul = false;
-          for(key in serialportsList) {
-            console.log(serialportsList[key].modul);
-            if (serialportsList[key].modul == newThing.modul){
-              Serialport.findById(serialportsList[key]._id, function (err, serialport) {
-                if (err) { return handleError(res, err); }
-                var updated = _.merge(serialport, newThing);
-                updated.save(function (err) {
-                  if (err) { return handleError(res, err); }
-                  return serialport;
-                });
-              });
-              return _modul = serialportsList[key].modul;
-            }
-          }
-          //console.log(_modul);
-          //console.log(serialportsList);
-          if(_modul == false) {
-            /*Serialport.create(newThing, function(err, serialport) {
-             if(err) { return handleError(res, err); }
-             return null;
-             });*/
-            console.log('create: ');
-          }
-        }
-      });
-      return;
-    }, 5 * 1 * 1000); // sec * min * faktor
-
-
+    }, 20 * 1 * 1000); // sec * min * faktor
   });
+
+
+    port.on('data', function(data) {
+      //console.log('data received: ' + data.toString('hex'));
+      var stringData = data.toString('hex');
+      /*
+       * var jsonData ist ein array also kann mit jsonData[0] auf die verschiedenen values zugegriffen werden.
+       */
+      //var jsonData = data.readInt8(0);
+      var jsonData = stringData.match(/.{1,2}/g);
+      //console.log('jsonData: ' + jsonData);
+
+      var key, count = 0;
+      for(key in jsonData) {
+        if(jsonData.hasOwnProperty(key)) {
+          dataArray.push(jsonData[count]);
+          count++;
+        }
+      }
+      //console.log('count: ' + count);
+      console.log(dataArray);
+      if(dataArray[11] != null && dataArray[12] == null) {
+        //console.log('Data2: ' + dataArray);
+        var newThing = {};
+        newThing.modul = dataArray[1];
+        newThing.type = dataArray[3];
+        newThing.value = dataArray;
+        var res;
+
+        loadSerialportsList();
+        //console.log(serialportsList);
+        var _modul = false;
+        for(key in serialportsList) {
+          //console.log('newThing: ' + newThing.modul);
+          //console.log('SerialPort: ' + serialportsList[key].modul);
+          if (serialportsList[key].modul == newThing.modul && serialportsList[key].type == newThing.type){
+            Serialport.findById(serialportsList[key]._id, function (err, serialport) {
+              //console.log('findById: ');
+              if (err) { return handleError(res, err); }
+              var updated = _.merge(serialport, newThing);
+              updated.save(function (err) {
+                if (err) { return handleError(res, err); }
+                return serialport;
+              });
+            });
+            return _modul = serialportsList[key].modul;
+          }
+        }
+        //console.log(_modul);
+        //console.log(serialportsList);
+        if(_modul == false) {
+          console.log('create: ');
+          Serialport.create(newThing, function(err, serialport) {
+           if(err) { return handleError(res, err); }
+           return null;
+           });
+
+        }
+      }
+    });
+
 
 
 
 
 
   /*port.open(function (err) {
-    if (err) {
-      return console.log('Error opening port: ', err.message);
-    }
-    // errors will be emitted on the port since there is no callback to write
-    // 02 = start, 01 = modul, 01 = modul, 00 = länge der Datenbits bei anfrage immer 00, 00 = Zählerstände Senden, 05 = TYP Anfrage
-    port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x00, 0x05]));
-    // new Buffer("250001000192CD0000002F6D6E742F72", "hex") auch möglich
-  });
+   if (err) {
+   return console.log('Error opening port: ', err.message);
+   }
+   // errors will be emitted on the port since there is no callback to write
+   // 02 = start, 01 = modul, 01 = modul, 00 = länge der Datenbits bei anfrage immer 00, 00 = Zählerstände Senden, 05 = TYP Anfrage
+   port.write(new Buffer([0x02, 0x01, 0x01, 0x00, 0x00, 0x05]));
+   // new Buffer("250001000192CD0000002F6D6E742F72", "hex") auch möglich
+   });
 
-  port.on('data', function(data) {
-    console.log('data received: ' + data.toString('hex'));
-  });*/
+   port.on('data', function(data) {
+   console.log('data received: ' + data.toString('hex'));
+   });*/
 })();
 
 // Get list of serialports
